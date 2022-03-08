4a7e6e7f1eb211d60947fb5016830c4953372641
kolagani renuka <kolagani renuka@LAPTOP-F7RL5C13>
2022-03-08 12:11:15 +0530

    This is my new branch created

---
+++ src/test/java/com/renu/R1.java
@@ -0,0 +1,10 @@
+package com.renu;
+
+public class R1 {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first msg");
+
+	}
+
+}
---
+++ src/test/java/com/renu/R2.java
@@ -0,0 +1,10 @@
+package com.renu;
+
+public class R2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is my second msg");
+
+	}
+
+}
